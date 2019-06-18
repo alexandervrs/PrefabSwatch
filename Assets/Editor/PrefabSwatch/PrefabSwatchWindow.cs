b41a772f672999f8c689cde4e5735674c769ab48
Alexander Vourtsis <alexander@vourtsis.com>
2019-06-19 02:44:48 +0300

    Added features

--- Assets/Editor/PrefabSwatch/PrefabSwatchWindow.cs
+++ Assets/Editor/PrefabSwatch/PrefabSwatchWindow.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine.SceneManagement;
+using UnityEditor.Events;
+
 
 public class PrefabSwatchWindow : EditorWindow
 {
@@ -21,11 +23,15 @@ public class PrefabSwatchWindow : EditorWindow
     }
 
     Transform parentTo;
+    Transform selectedParent;
 
     bool snap = true;
     Vector2 snapCellSize = new Vector2(1, 1);
 
     string nameSuffix = "";
+
+    bool continuousPlacement = false;
+    bool deleteOverlapping = false;
 
     bool overrideOptions = false;
 
@@ -51,9 +57,26 @@ public class PrefabSwatchWindow : EditorWindow
 
     int defaultSortingLayerIDSelected;
 
+    bool restrictDeleteToLayer = false;
+
+    int deleteSortingLayerID;
+    int deleteSortingOrder = 0;
+
+    int deleteSortingLayerIDSelected;
+
+    Color originalColor;
+
+    int currentUniqueID;
+
+    Vector2 mousePos = Vector2.zero;
+
     float listPreviewZoom = 4.0f;
 
+    List<GameObject> hasNotBoxCollider = new List<GameObject>();
+
     GameObject selectedPrefab;
+
+    GameObject prefabToRemove;
 
     Vector2 prefabsScrollView;
     Vector2 mousePosition;
@@ -108,8 +131,8 @@ public class PrefabSwatchWindow : EditorWindow
 
     void OnEnable()
     {
-        SceneView.onSceneGUIDelegate -= OnSceneGUI;
-        SceneView.onSceneGUIDelegate += OnSceneGUI;
+        SceneView.duringSceneGui -= OnSceneGUI;
+        SceneView.duringSceneGui += OnSceneGUI;
 
         Undo.undoRedoPerformed -= Repaint;
         Undo.undoRedoPerformed += Repaint;
@@ -123,6 +146,10 @@ public class PrefabSwatchWindow : EditorWindow
         layerID = GetSortingLayerUniqueIDs();
 
         defaultSortingLayerID = SortingLayer.GetLayerValueFromName("Default");
+
+        snapCellSize.x = EditorPrefs.GetFloat("Editor_PrefabSwatch_SnapX");
+        snapCellSize.y = EditorPrefs.GetFloat("Editor_PrefabSwatch_SnapY");
+
 
     }
 
@@ -169,10 +196,11 @@ public class PrefabSwatchWindow : EditorWindow
         GUIStyle guiSeparator = new GUIStyle("box");
 
         // parent to
-        Transform selectedParent = EditorGUILayout.ObjectField("Parent To", parentTo, typeof(Transform), true) as Transform;
+        selectedParent = EditorGUILayout.ObjectField("Parent To", parentTo, typeof(Transform), true) as Transform;
         if (selectedParent != parentTo) {
             if (selectedParent == null || (PrefabUtility.GetCorrespondingObjectFromSource(selectedParent) == null && PrefabUtility.GetPrefabInstanceHandle(selectedParent) == null)) {
                 parentTo = selectedParent;
+                selectedParent = null;
             }
         }
 
@@ -268,15 +296,60 @@ public class PrefabSwatchWindow : EditorWindow
 
         }
 
+        // --- separator ---
+        guiSeparator.border.top     = guiSeparator.border.bottom  = 1;
+        guiSeparator.margin.top     = guiSeparator.margin.bottom  = 5;
+        guiSeparator.margin.bottom  = guiSeparator.margin.top     = 5;
+        guiSeparator.padding.top    = guiSeparator.padding.bottom = 1;
+        GUILayout.Box("", guiSeparator, GUILayout.ExpandWidth(true), GUILayout.Height(1));
+
         // pixel snap
         EditorGUILayout.BeginHorizontal();
             EditorGUILayout.PrefixLabel("Snap");
+            GUI.Label(GUILayoutUtility.GetLastRect(), new GUIContent("", "Enabling this option will snap the Objects to the specified Cell size (in Units)"));
             snap = EditorGUILayout.Toggle(snap, GUILayout.Width(15f));
-            GUI.enabled = snap;
+        EditorGUILayout.EndHorizontal();
+            if (snap) {
             snapCellSize = EditorGUILayout.Vector2Field("", snapCellSize);
             snapCellSize = Vector2.Max(snapCellSize, new Vector2(0.1f, 0.1f));
-            GUI.enabled = true;
-        EditorGUILayout.EndHorizontal();
+            }
+        
+
+        EditorGUILayout.Space();
+
+        // continuous placement
+        continuousPlacement = EditorGUILayout.Toggle("Continuous Placement", continuousPlacement);
+        GUI.Label(GUILayoutUtility.GetLastRect(), new GUIContent("", "Enabling this option will continuously place Prefabs when you click and drag the mouse"));
+
+        // delete overlapping
+        deleteOverlapping = EditorGUILayout.Toggle("Delete Overlapping", deleteOverlapping);
+        GUI.Label(GUILayoutUtility.GetLastRect(), new GUIContent("", "Enabling this option will delete any GameObjects with the same name as the one placed when overlapping. If the GameObject is missing a BoxCollider2D Component, it will be auto-added. Deactivate this option or add a BoxCollider2D Component yourself if you don't want this behavior"));
+
+        EditorGUILayout.Space();
+
+        // delete on sorting layer
+        restrictDeleteToLayer = EditorGUILayout.Toggle("Restrict Delete", restrictDeleteToLayer);
+        GUI.Label(GUILayoutUtility.GetLastRect(), new GUIContent("", "Enabling this option will restrict the deletion functionality with right click only to a specific sorting layer and order in that layer"));
+
+        if (restrictDeleteToLayer) {
+
+            EditorGUILayout.BeginVertical();
+
+            if (!SortingLayer.IsValid(deleteSortingLayerID)) {
+                deleteSortingLayerID = SortingLayer.GetLayerValueFromName("Default");
+                deleteSortingLayerIDSelected = 0;
+            }
+
+            deleteSortingLayerIDSelected = EditorGUILayout.Popup("Sorting Layer", deleteSortingLayerIDSelected, layerNames);
+            deleteSortingLayerID = layerID[deleteSortingLayerIDSelected];
+
+            deleteSortingOrder = EditorGUILayout.IntField("Order in Layer", deleteSortingOrder);
+
+            EditorGUILayout.EndVertical();
+
+        }
+
+        EditorGUILayout.Space();
 
         // --- separator ---
         guiSeparator.border.top     = guiSeparator.border.bottom  = 1;
@@ -380,6 +453,10 @@ public class PrefabSwatchWindow : EditorWindow
 
             }
 
+            EditorPrefs.SetFloat("Editor_PrefabSwatch_SnapX", snapCellSize.x);
+            EditorPrefs.SetFloat("Editor_PrefabSwatch_SnapY", snapCellSize.y);
+
+            
 
         }
 
@@ -435,7 +512,7 @@ public class PrefabSwatchWindow : EditorWindow
         Transform t = prefabToPlaceInScene.transform;
         prefabToPlaceInScene.hideFlags = HideFlags.None;
 
-        Undo.RegisterCreatedObjectUndo(prefabToPlaceInScene, "Place Prefab \""+Regex.Replace(selectedPrefab.name, "(?!^)([A-Z])", " $1")+"\"");
+        prefabToPlaceInScene.GetComponent<SpriteRenderer>().color = originalColor;
 
         if (nameSuffix != "") {
             prefabToPlaceInScene.gameObject.name = prefabToPlaceInScene.gameObject.name+nameSuffix;
@@ -443,6 +520,8 @@ public class PrefabSwatchWindow : EditorWindow
 
         prefabToPlaceInScene = null;
         UpdatePlacingPrefab();
+
+        Undo.RegisterCreatedObjectUndo(prefabToPlaceInScene, "Place Prefab \""+Regex.Replace(selectedPrefab.name, "(?!^)([A-Z])", " $1")+"\"");
 
         if (parentTo != null) {
             Vector3    pos = t.localPosition;
@@ -454,9 +533,85 @@ public class PrefabSwatchWindow : EditorWindow
 
     }
 
+
+    void RemovePrefab() {
+
+        Vector2 positionToRemoveAt = mousePosition;
+
+        GameObject prefabToRemove = HandleUtility.PickGameObject(positionToRemoveAt, true);
+
+        if (prefabToRemove != null && prefabToRemove.GetComponent<Camera>() == null) {
+            
+            if (restrictDeleteToLayer) {
+
+                if (
+                    prefabToRemove.GetComponent<SpriteRenderer>().sortingLayerID == deleteSortingLayerID &&
+                    prefabToRemove.GetComponent<SpriteRenderer>().sortingOrder == deleteSortingOrder
+                    ) {
+                    DestroyImmediate(prefabToRemove);
+                    prefabToRemove = null;
+                }
+
+            } else {
+            
+                DestroyImmediate(prefabToRemove);
+                prefabToRemove = null;
+
+            }
+
+        }
+
+    }
+
+
+    void RemoveOverlappedPrefab() {
+
+        if (!deleteOverlapping) {
+            return;
+        }
+
+        var ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+        var hits = Physics2D.GetRayIntersectionAll(ray, float.MaxValue);
+        var nearest = new RaycastHit2D();
+
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+
+        foreach(GameObject go in allObjects)
+        {
+
+            if (go.GetComponent<BoxCollider2D>() == null) {
+                go.AddComponent<BoxCollider2D>();
+                hasNotBoxCollider.Add(go);
+            }
+
+        }  
+
+        if (hits.Length > 0)
+        {
+            float nearestDist = float.MaxValue;
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.gameObject == prefabToPlaceInScene || hit.collider.transform.IsChildOf(prefabToPlaceInScene.transform) || hit.collider.gameObject == Camera.main) {
+                   
+                    continue;
+                }
+                if (hit.distance < nearestDist)
+                {
+                    nearestDist = hit.distance;
+                    nearest = hit;
+                    DestroyImmediate(hit.collider.gameObject);
+                    
+                }
+            }
+        }
+
+
+    }
+
+
     void UpdatePlacingPrefab()
     {
-        
+
         if (prefabToPlaceInScene != null) {
             GameObject prefab = (GameObject)PrefabUtility.GetCorrespondingObjectFromSource(prefabToPlaceInScene);
             if (selectedPrefab != prefab) {
@@ -465,8 +620,18 @@ public class PrefabSwatchWindow : EditorWindow
         }
 
         if (prefabToPlaceInScene == null && selectedPrefab != null) {
+
             prefabToPlaceInScene = (GameObject)PrefabUtility.InstantiatePrefab(selectedPrefab, SceneManager.GetActiveScene());
             prefabToPlaceInScene.hideFlags = HideFlags.HideAndDontSave | HideFlags.NotEditable;
+
+            originalColor = prefabToPlaceInScene.GetComponent<SpriteRenderer>().color;
+
+            prefabToPlaceInScene.GetComponent<SpriteRenderer>().color = new Color(
+                prefabToPlaceInScene.GetComponent<SpriteRenderer>().color.r,
+                prefabToPlaceInScene.GetComponent<SpriteRenderer>().color.g,
+                prefabToPlaceInScene.GetComponent<SpriteRenderer>().color.b,
+                0.6f
+            );
         }
 
         if (prefabToPlaceInScene == null) {
@@ -505,7 +670,7 @@ public class PrefabSwatchWindow : EditorWindow
         if (spr != null) {
 
             if (overrideColor) {
-                spr.color = defaultColor;
+                originalColor = defaultColor;
             }
             if (overrideMaterial) {
                 spr.material = defaultMaterial;
@@ -534,6 +699,7 @@ public class PrefabSwatchWindow : EditorWindow
         int control = GUIUtility.GetControlID(FocusType.Passive);
 
         HandleUtility.Repaint();
+        Handles.color = Color.yellow;
 
         if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Escape) {
             DeselectPrefab();
@@ -552,30 +718,61 @@ public class PrefabSwatchWindow : EditorWindow
         switch (Event.current.type)
         {
             case EventType.Layout:
+
                 HandleUtility.AddDefaultControl(control);
+
                 break;
+
             case EventType.MouseDown:
+
                 if (Event.current.button == 0) {
                     Tools.current = Tool.None;
-                    Event.current.Use();
                     PlacePrefab();
+                    RemoveOverlappedPrefab();
+                    Event.current.Use();
+                    
                 }
+                else if (Event.current.button == 1) {
+                    RemovePrefab();
+                    Event.current.Use();
+                }
+
                 break;
+
+            case EventType.MouseDrag:
+
+                if (Event.current.button == 0 && continuousPlacement) {
+                    
+                    Tools.current = Tool.None;
+                    PlacePrefab();
+                    RemoveOverlappedPrefab();
+                    Event.current.Use();
+                }
+                else if (Event.current.button == 1 && continuousPlacement) {
+                    RemovePrefab();
+                    Event.current.Use();
+                }
+
+            break;
+
             case EventType.MouseUp:
+
                 if (Event.current.button == 0) {
                     Tools.current = Tool.None;
                     Event.current.Use();
                 }
+
                 break;
         }
 
         if (prefabToPlaceInScene != null) {
             Handles.CircleHandleCap(control, positionToPlaceAt, Quaternion.identity, 0.05f, EventType.Repaint);
+            DebugDrawBbox(prefabToPlaceInScene, Color.yellow);
         }
 
         Handles.BeginGUI();
 
-        GUILayout.BeginArea(new Rect(4f, 4f, 300f, EditorGUIUtility.singleLineHeight * 3f));
+        GUILayout.BeginArea(new Rect(4f, 4f, view.position.width, view.position.height));
 
         Rect rect = GUILayoutUtility.GetRect(300f, EditorGUIUtility.singleLineHeight);
         GUI.Label(rect, "X: " + positionToPlaceAt.x.ToString("0.00"), EditorStyles.whiteLabel);
@@ -585,6 +782,13 @@ public class PrefabSwatchWindow : EditorWindow
         rect = GUILayoutUtility.GetRect(300f, EditorGUIUtility.singleLineHeight);
         rect.y -= 8f;
         GUI.Label(rect, "Z: " + defaultZ.ToString("0.00"), EditorStyles.whiteLabel);
+
+        rect.y = 0;
+        rect.x = rect.width-160f;
+        Color defaultColor = GUI.color;
+        GUI.color = Color.yellow;
+        GUI.Label(rect, "ESC to exit Editing Mode", EditorStyles.whiteLabel);
+        GUI.color = defaultColor;
 
         GUILayout.EndArea();
 
@@ -602,6 +806,51 @@ public class PrefabSwatchWindow : EditorWindow
         Type internalEditorUtilityType = typeof(InternalEditorUtility);
         PropertyInfo sortingLayerUniqueIDsProperty = internalEditorUtilityType.GetProperty("sortingLayerUniqueIDs", BindingFlags.Static | BindingFlags.NonPublic);
         return (int[])sortingLayerUniqueIDsProperty.GetValue(null, new object[0]);
+    }
+
+    void DebugDrawBbox(GameObject go, Color color)
+    {
+
+        SpriteRenderer sprRenderer = go.GetComponent<SpriteRenderer>();
+        Vector3 position           = go.transform.position;
+        Vector3 scale              = go.transform.localScale;
+
+        Vector2 anchor = new Vector2( 
+                                (sprRenderer.sprite.pivot.x/sprRenderer.sprite.pixelsPerUnit)*scale.x,
+                                (sprRenderer.sprite.pivot.y/sprRenderer.sprite.pixelsPerUnit)*scale.y
+                            );
+
+        Vector2 size = new Vector2(
+                                (sprRenderer.sprite.texture.width/sprRenderer.sprite.pixelsPerUnit)*scale.x,
+                                (sprRenderer.sprite.texture.height/sprRenderer.sprite.pixelsPerUnit)*scale.y
+                            );
+
+        // top left
+        float x1 = position.x-anchor.x;
+        float y1 = position.y-anchor.y+size.y;
+        Vector2 topLeft = new Vector2(x1, y1);
+
+        // top right
+        float x2 = position.x-anchor.x+size.x;
+        float y2 = position.y-anchor.y+size.y;
+        Vector2 topRight = new Vector2(x2, y2);
+
+        // bottom
+        float x3 = position.x-anchor.x + size.x;
+        float y3 = position.y-anchor.y;
+        Vector2 bottomRight = new Vector2(x3, y3);
+
+        // left
+        float x4 = position.x-anchor.x;
+        float y4 = position.y-anchor.y;
+        Vector2 bottomLeft = new Vector2(x4, y4);
+
+
+        Handles.DrawLine(topLeft, topRight);
+        Handles.DrawLine(topRight, bottomRight);
+        Handles.DrawLine(bottomRight, bottomLeft);
+        Handles.DrawLine(bottomLeft, topLeft);
+
     }
 
 }
